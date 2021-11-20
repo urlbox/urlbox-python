abaaad3ec805395c911af9f7b336885854309301
Alan Donohoe <alandonohoe@hotmail.com>
2021-11-20 15:18:15 +0000

    Add basic get functionality to the UrlboxClient class.
    So now users of this package can make simple get requests to the
    Urlbox API, using their API key, url they want to take a screenshot of
    and the correct format and options.
    
    ref:
    https://urlbox.io/docs/getting-started

--- tests/test_urlbox_client.py
+++ tests/test_urlbox_client.py
@@ -1,31 +1,25 @@
 from faker import Faker
 from urlbox import UrlboxClient
 import pytest
+import random
+import requests
+import requests_mock
+import urllib.parse
+
 
 fake = Faker()
 
-# Fixtures
-@pytest.fixture
-def api_key():
-    return fake.pystr()
-
-
-@pytest.fixture
-def api_secret():
-    return fake.pystr()
-
-
-# Tests
 # test_init()
-
-
 def test_only_api_key_provided():
+    api_key = fake.pystr()
     urlbox_client = UrlboxClient(api_key=api_key)
 
     assert urlbox_client.api_key == api_key
 
 
 def test_both_api_key_and_api_secret_provided():
+    api_key = fake.pystr()
+    api_secret = fake.pystr()
     urlbox_client = UrlboxClient(api_key=api_key, api_secret=api_secret)
 
     assert (
@@ -42,3 +36,51 @@ def test_api_key_not_provided():
         str(type_error.value)
         == "__init__() missing 1 required keyword-only argument: 'api_key'"
     )
+
+
+# Test get()
+# valid api key
+# valid format and options
+def test_successful_request():
+    api_key = fake.pystr()
+
+    format = random.choice(
+        ["png", "jpg", "jpeg", "avif", "webp", "pdf", "svg", "html"]
+    )
+    options = {
+        "full_page": random.choice([True, False]),
+        "width": fake.random_int(),
+    }
+    url = fake.url()
+
+    urlbox_request_url = (
+        f"{UrlboxClient.URLBOX_BASE_API_URL}"
+        f"{api_key}/{format}"
+        f"?url={urllib.parse.quote(url)}"
+        f"&{urllib.parse.urlencode(options)}"
+    )
+
+    urlbox_client = UrlboxClient(api_key=api_key)
+
+    with requests_mock.Mocker() as requests_mocker:
+        with open(
+            "tests/files/urlbox_screenshot.png", "rb"
+        ) as urlbox_screenshot:
+            requests_mocker.get(
+                urlbox_request_url,
+                content=urlbox_screenshot.read(),
+                headers={"content-type": f"image/{format}"},
+            )
+
+            response = urlbox_client.get(url, format=format, options=options)
+
+            assert response.status_code == 200
+            assert format in response.headers["Content-Type"]
+            assert isinstance(response, requests.models.Response)
+            assert isinstance(response.content, bytes)
+
+
+# TODO:
+# Test invalid URL
+# Test URL with white spaces
+# Test invalid API key
